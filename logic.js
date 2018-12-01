f232aae9bd25884f496fc55d77474a3730325f9f
Thomas Murphy <Tom@Thomass-MacBook-Air.local>
2018-12-01 17:18:50 +0100

    name picker

--- logic.js
+++ logic.js
@@ -28,9 +28,9 @@ const stopButton = document.getElementById('stopButton');
 // Start or stop the name shuffle on button click
 startButton.addEventListener('click', function() {
 	this.style.display = 'none';
-	stopButton.style.display = 'block';
+	stopButton.style.display = 'inline';
 	intervalHandle = setInterval(function() {
-		headerNames.textContent = namesList[i++ % namesList.length];
+		names.textContent = namesList[i++ % namesList.length];
 	}, 50);
 	if (showTimer === true) {
 		timerWrapper.classList.remove('visible');
@@ -39,7 +39,7 @@ startButton.addEventListener('click', function() {
 
 stopButton.addEventListener('click', function() {
 	this.style.display = "none";
-	startButton.style.display = 'block';
+	startButton.style.display = 'inline';
 	clearInterval(intervalHandle);
 	timer.innerHTML = time;
 	if (showTimer === true) {
@@ -47,29 +47,3 @@ stopButton.addEventListener('click', function() {
 	}
 	startTimer();
 });
-
-// // Allow use of spacebar to start/stop name shuffle
-// document.body.onkeyup = function(e) {
-// 	if (e.keyCode == 32) {
-// 		if (x % 2 === 0) {
-// 			startButton.style.display = "none";
-// 			stopButton.style.display = "block";
-// 			intervalHandle = setInterval(function() {
-// 				headerNames.textContent = namesList[i++ % namesList.length];
-// 			}, 50);
-// 			if (showTimer === true) {
-// 				timerWrapper.classList.remove('visible');
-// 			}
-// 		} else {
-// 			startButton.style.display = "block";
-// 			stopButton.style.display = "none";
-// 			clearInterval(intervalHandle);
-// 			timer.innerHTML = time;
-// 			if (showTimer === true) {
-// 				timerWrapper.classList.add('visible');
-// 			}
-// 			startTimer();
-// 		}
-// 		x++;
-// 	}
-// }
