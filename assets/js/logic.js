63d8845f23c5bd0c6b690c43e9319886ce7e1473
Thomas Murphy <Tom@Thomass-MacBook-Air.local>
2018-12-02 10:51:55 +0100

    clean

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,4 @@
-// Add list of names here
+// Add list of names
 const nameList = [
 	'Matt',
 	'Brendan',
@@ -19,32 +19,38 @@ const nameList = [
 let i = 0;
 let x = 0;
 let intervalHandle = null;
-let showTimer = null;
-let timer = null;
-let time = 0;
+// let showTimer = null;
+// let timer = null;
+// let time = 0;
+
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
-const headerOne = document.getElementById('headerNames');
+const name = document.getElementById('names');
 
 // Start or stop the name shuffle on button click
 startButton.addEventListener('click', function() {
 	this.style.display = 'none';
 	stopButton.style.display = 'inline';
+	
 	intervalHandle = setInterval(function() {
 		names.textContent = nameList[i++ % nameList.length];
 	}, 50);
-	if (showTimer === true) {
-		timerWrapper.classList.remove('visible');
-	}
+
+	// if (showTimer === true) {
+	// 	timerWrapper.remove('visible');
+	// }
 });
 
 stopButton.addEventListener('click', function() {
 	this.style.display = "none";
 	startButton.style.display = 'inline';
+
 	clearInterval(intervalHandle);
-	timer.innerHTML = time;
-	if (showTimer === true) {
-		timerWrapper.classList.add('visible');
-	}
-	startTimer();
+	// timer.innerHTML = time;
+
+	// if (showTimer === true) {
+	// 	timerWrapper.add('visible');
+	// }
+
+	// startTimer();
 });
